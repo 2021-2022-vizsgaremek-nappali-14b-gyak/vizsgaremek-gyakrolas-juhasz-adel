e18c3fe7704a84851d6e4ca4f1328810f98f7d28
sz0juhade <sz0juhade@vasvari.org>
2021-12-02 11:58:37 +0100

    Navigation osztály létrejött.

--- VizsgaremekProjekt/obj/Release/net5.0-windows/MainWindow.g.cs
+++ VizsgaremekProjekt/obj/Release/net5.0-windows/MainWindow.g.cs
@@ -101,7 +101,7 @@ namespace VizsgaremekProjekt {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/VizsgaremekProjekt;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/VizsgaremekProjekt;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
