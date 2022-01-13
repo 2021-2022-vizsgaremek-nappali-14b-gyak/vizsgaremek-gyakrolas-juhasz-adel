2a90dd59936b15e33ad0e425e557105b80cca71a
sz0juhade <sz0juhade@vasvari.org>
2022-01-13 08:31:41 +0100

    DataBaseSource

---
+++ VizsgaremekProjekt/Models/DataBaseSource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VizsgaremekProjekt.Models
+{
+    public enum DbSource { NONE, LOCALHOST, DEVOPS}
+    public class DataBaseSource { 
+
+            DbSource dbSource;
+
+            public DbSource DbSource { get => dbSource; set => dbSource = value; }
+
+            public DataBaseSource()
+            {
+                this.DbSource = DbSource.LOCALHOST;
+            }
+
+        }
+    }
